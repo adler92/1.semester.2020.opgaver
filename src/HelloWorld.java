05f2a4ecc2853a60949873a80df0dde6bbd522f8
Lukas <luka0718@edu.easj.dk>
2020-09-07 12:00:52 +0200

    Tester om repository virker

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    // Første Programmerings time hvor vi lære at skrive "Hello World"
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World");
+
+        // Jeg leger bare med noget ekstra
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Skriv noget!");
+        String str = sc.nextLine();
+        System.out.println(str);
+        System.out.println("Smart ikke?");
+
+    }
+}
